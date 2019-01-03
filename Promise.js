6a02ec9a68305086a89d690e1b758daa207eb7cd
yeyan1996 <1366792281@qq.com>
2019-01-03 23:34:00 +0800

    doc:添加了一些注释

--- Promise.js
+++ Promise.js
@@ -2,31 +2,24 @@ const PENDING = 'pending'
 const RESOLVED = 'resolved'
 const REJECTED = 'rejected'
 
-const isComplexDataType = obj => {
-    return (typeof obj === 'object' || typeof obj === 'function') && (obj !== null)
-}
 
 //决议Promise
 function resolvePromise(promise2, value, resolve, reject) {
-    //排除可能既调resolve又调reject情况
-    // 表示是否调用过成功或者失败
+    //排除可能既调resolve又调用reject情况
+    // 表示是否已经决议
     let called;
-    // 如果循环引用报错,则reject
+    // 如果循环引用则reject
     if (value === promise2) {
         return reject(new TypeError('Chaining cycle detected for promise'));
     }
 
-    if (!value || !isComplexDataType) {
-        //value为普通值
-        //将promise2状态改为resolve(外层函数会返回这个resolved状态的promise2)
-        resolve(value)
-        return
-    }
+
     //value为thenable对象(会被认为是个Promise)
-    //如果 value 处于等待态，Promise 需保持为等待态直至 value 被执行或拒绝
+    //如果 value 处于pending，Promise 需保持为等待态直至 value 被执行或拒绝
     //如果 value 处于其他状态，则用相同的值处理 Promise
-    if (value.then && typeof value.then === 'function') {
+    if (value && value.then && typeof value.then === 'function') {
         value.then(
+            // 传入onFulfilled/onRejected函数（此时这2个函数未执行）
             function onFulfilled(res) {
                 if (called) return;
                 called = true;
@@ -66,7 +59,7 @@ class MyPromise {
         }
         this.status = PENDING
         this.value = undefined
-        // 存储多个then方法的所有回调
+        // 存储多个then方法时的所有回调
         // promise.then(res1=>{代码1....})
         // promise.then(res2=>{代码2....})
         // promise.then(res3=>{代码3....})
@@ -82,32 +75,31 @@ class MyPromise {
     }
 
     then(onFulfilled, onRejected) {
-        //如果then参数不是function就使用默认的函数传递Promise链
+        //如果then参数不是function就使用默认的函数继续向后传递Promise链
         (typeof onFulfilled === 'function') || (onFulfilled = value => value)
         (typeof onRejected === 'function') || (onRejected = value => {throw value})
 
-        //将then/catch的返回值包装成一个Promise,遵循PromiseA+规范
+        //将then/catch的返回值包装成一个Promise
         let promise2 = new MyPromise((resolve, reject) => {
             switch (this.status) {
 
                 case PENDING: {
-                    // 如果Promise还没有决议,则将相应的回调放入数组存储,等待resolve/reject执行
+                    // 如果Promise还没有决议,则将相应的回调放入数组存储,等待resolve/reject执行后将回调放入微任务队列
                     //这里使用setTimeout(()=>{},0)模拟微任务
-                    //Todo 这里pending状态需要用异步么?
-                    setTimeout(() => {
-                        this.resolvedCallbacks.push(
-                            // 放入了回调函数,但这时候this.value值还是undefined
-                            () => {
-                                onFulfilled(this.value)
-                            }
+
+                    this.resolvedCallbacks.push(
+                        //放入了一个函数，等待执行（执行这个箭头函数等同于执行onFulfilled/onRejected函数）
+                        // 放入了回调函数,但这时候this.value值还是undefined
+                        () => {
+                            onFulfilled(this.value)
+                        }
                     )
-                        this.rejectedCallbacks.push(
-                            () => {
-                                onRejected(this.value)
-                            }
-                        )
-                        // resolvePromise(promise2, x, resolve, reject)
-                    })
+                    this.rejectedCallbacks.push(
+                        () => {
+                            onRejected(this.value)
+                        }
+                    )
+                    // resolvePromise(promise2, x, resolve, reject)
                     break;
                 }
 
@@ -119,6 +111,7 @@ class MyPromise {
                             let res = onFulfilled(this.value)
                             resolvePromise(promise2, res, resolve, reject)
                         } catch (e) {
+                            //如果在onFulFilled/onRejected中发生了异常，用异常信息作为值，将promise2的状态变为rejected
                             reject(e)
                         }
                     })
@@ -140,6 +133,7 @@ class MyPromise {
             }
         })
         // debugger
+        //返回一个promise
         return promise2
     }
 
